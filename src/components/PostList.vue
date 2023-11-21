c25f7c1662a0a921469c64c473f65ba89694936f
Terézia Boďová <118959897+TerezzaB@users.noreply.github.com>
2023-11-21 14:26:07 +0100

    First commit

---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="post-list">
+        <div v-for="post in posts" :key="post.id">
+            <SinglePost :post="post"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from 'vue';
+import SinglePost from './SinglePost.vue'
+
+export default {
+    name: 'PostList',
+    props: ['posts'],
+    components: { SinglePost },
+    setup(props) {
+        onMounted(() => console.log('Component PostList mounted'))
+        onUnmounted(() => console.log('Component UNmounted'))
+        onUpdated(() => console.log('Component updated'))
+    }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home Page</h1>
+    <PostList v-if="showPosts" :posts="posts" /> 
+    <button @click="showPosts = !showPosts">Toggle Posts</button>
+    <button @click="posts.pop()">Delete a Post</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import { ref } from 'vue'
+import PostList from '../components/PostList.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components: { PostList },
+  setup() {
+    //will be run before every life cycle hooks (mounted/created etc)
+    const posts = ref([
+      { title: 'Welcome to the blog', body: 'Lorem ipsum body', id: 1 },
+      { title: 'Top 5 CSS tips', body: 'Lorem ipsum body 2', id: 2 },
+      { title: 'Top 6 CSS tips', body: 'Lorem ipsum body 3', id: 3 },
+      { title: 'Top 7 CSS tips', body: 'Lorem ipsum body 4', id: 4 },
+      { title: 'Top 8 CSS tips', body: 'Lorem ipsum body 5', id: 5 }
+    ])
+
+    const showPosts = ref(false)
+
+    return { posts, showPosts }
   }
 }
 </script>
