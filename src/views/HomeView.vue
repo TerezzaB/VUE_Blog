8a1e71a3f961c58287c61d9d5c6808a537a75af9
Terézia Boďová <118959897+TerezzaB@users.noreply.github.com>
2023-11-22 12:31:16 +0100

    2nd commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="home">
     <h1>Home Page</h1>
-    <PostList v-if="showPosts" :posts="posts" /> 
-    <button @click="showPosts = !showPosts">Toggle Posts</button>
-    <button @click="posts.pop()">Delete a Post</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />  
+    </div>
+    <div v-else>Loading ...</div>
   </div>
 </template>
 
@@ -18,17 +20,25 @@ export default {
   components: { PostList },
   setup() {
     //will be run before every life cycle hooks (mounted/created etc)
-    const posts = ref([
-      { title: 'Welcome to the blog', body: 'Lorem ipsum body', id: 1 },
-      { title: 'Top 5 CSS tips', body: 'Lorem ipsum body 2', id: 2 },
-      { title: 'Top 6 CSS tips', body: 'Lorem ipsum body 3', id: 3 },
-      { title: 'Top 7 CSS tips', body: 'Lorem ipsum body 4', id: 4 },
-      { title: 'Top 8 CSS tips', body: 'Lorem ipsum body 5', id: 5 }
-    ])
+    const posts = ref([])
+    const error = ref(null)
+  
+    const load = async () => {
+      try {
+        let data = await fetch('http://localhost:3000/posts')
+        if (!data.ok) {
+          throw Error('No Data available')
+        }
+        posts.value = await data.json()
+      }
+      catch(err) {
+        error.value = err.message
+        console.log(error.value)
+      }
+    }
 
-    const showPosts = ref(false)
-
-    return { posts, showPosts }
+    load()
+    return { posts, error }
   }
 }
 </script>
